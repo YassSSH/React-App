186d3116ea1c96cb2f95c3a0d4a64913febd75bf
YassSSH <yassine.regragui@live.fr>
2022-02-12 01:18:18 +0100

    Start

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, dolore consequatur dolor inventore velit error minus totam atque ratione iusto nostrum cum voluptates ipsa doloribus. Cumque commodi necessitatibus laborum nam.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, fuga quia tempora quibusdam expedita nulla esse perferendis explicabo laborum laboriosam quos dolores exercitationem natus, maiores quaerat nam, voluptatibus soluta fugit.</p>
+            
+        </div>
+    );
+};
+
+export default About;
